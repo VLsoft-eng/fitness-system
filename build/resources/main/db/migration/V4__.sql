c063db2d713257c5cc91f0cd899ef299cb2d6f72
Vladislav Karpuk <karpuk-05@mail.ru>
2025-06-21 16:23:32 +0700

    remove trash files

--- build/resources/main/db/migration/V4__.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE posts
-(
-    id          BIGSERIAL PRIMARY KEY,
-    title       VARCHAR(255) NOT NULL,
-    description TEXT,
-    image_base64 TEXT,
-    created_at  TIMESTAMP    NOT NULL,
-    updated_at  TIMESTAMP
-);
