02779cd8f3c06d98b4cb4c7017cead8f28e84721
VLsoft-eng <deeeaddrop@gmail.com>
2025-06-27 01:25:34 +0700

    feat: changes

--- src/main/resources/db/migration/V13__.sql
+++ src/main/resources/db/migration/V13__.sql
@@ -1,12 +1,12 @@
+-- Удаление всех записей из промежуточной таблицы training_session_full_exercise
+DELETE FROM training_session_full_exercise;
+
+-- Удаление всех записей из таблицы full_exercises
+DELETE FROM full_exercises;
+
 -- Добавление столбца trainer_id в таблицу full_exercises
 ALTER TABLE full_exercises
     ADD COLUMN trainer_id BIGINT NOT NULL;
-
--- Заполнение trainer_id на основе trainer_id из таблицы exercises
-UPDATE full_exercises fe
-SET trainer_id = e.trainer_id
-    FROM exercises e
-WHERE fe.exercise_id = e.id;
 
 -- Добавление внешнего ключа для trainer_id
 ALTER TABLE full_exercises
