519fc48585e8db749886b67e3ebc30b3cdf3c0c8
VLsoft-eng <deeeaddrop@gmail.com>
2025-06-27 23:25:38 +0700

    feat: changes

--- src/main/java/ru/hits/fitnesssystem/core/config/RestTemplateConfig.java
+++ src/main/java/ru/hits/fitnesssystem/core/config/RestTemplateConfig.java
@@ -1,11 +1,14 @@
 package ru.hits.fitnesssystem.core.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
 public class RestTemplateConfig {
@@ -20,6 +23,8 @@ public class RestTemplateConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new Jdk8Module());
         mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
         return mapper;
     }
 }
